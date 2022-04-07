3f064929b86fa02c8c8e202cdf727ddb75d23f02
teszerrakt <zaky.syihab@gmail.com>
2022-04-07 23:33:20 +0700

    feat: add scrollbar styling to textarea

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -3,46 +3,51 @@ import styles from './Contact.module.scss'
 const Contact = () => {
   return (
     <section id='contact' className={styles.contactSection}>
-      <form
-        name='contact-form'
-        method='POST'
-        className={styles.contactForm}
-        data-netlify='true'
-      >
+      <div className={styles.contactContainer}>
         <div className={`${styles.title} text-gradient`}>
           Contact Me
         </div>
-        <input type='hidden' name='form-name' value='contact-form'/>
-        <div className={styles.inputContainer}>
-          <label htmlFor='name'>Name</label>
-          <input
-            type='text'
-            id='name'
-            className={styles.blue}
-          />
-        </div>
-        <div className={styles.inputContainer}>
-          <label htmlFor='email'>Email</label>
-          <input
-            type='email'
-            id='email'
-            className={styles.red}
-          />
-        </div>
-        <div className={styles.inputContainer}>
-          <label htmlFor='message'>Message</label>
-          <textarea
-            id='message'
-            className={styles.blue}
-          />
-        </div>
-        <button
-          type='submit'
-          className={`${styles.btnSubmit} bg-gradient`}
+        <form
+          name='contact-form'
+          method='POST'
+          className={styles.contactForm}
+          data-netlify='true'
         >
-          Submit
-        </button>
-      </form>
+          <input type='hidden' name='form-name' value='contact-form' />
+          <div className={styles.inputContainer}>
+            <label htmlFor='name'>Name</label>
+            <input
+              type='text'
+              id='name'
+              name='name'
+              className={styles.blue}
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              id='email'
+              name='name'
+              className={styles.red}
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <label htmlFor='message'>Message</label>
+            <textarea
+              id='message'
+              name='name'
+              className={`${styles.blue} scrollbar`}
+            />
+          </div>
+          <button
+            type='submit'
+            className={`${styles.btnSubmit} bg-gradient`}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </section>
   )
 }
